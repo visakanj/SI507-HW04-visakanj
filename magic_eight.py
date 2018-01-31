f3eb8b280aeb7ba6cea1620ab7345634617130db
Visakan Jayakumar <visakanj@visakans-mbp.attlocal.net>
2018-01-31 13:01:24 -0500

    check question code

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,8 @@
 def ask_user():
 	user_input = input("What is your question?")
+
+	if user_input[-1] != "?":
+		print("I'm sorry, I can only answer questions.")
+
+	while user_input != "quit":
+		user_input = input("What is your question?")
